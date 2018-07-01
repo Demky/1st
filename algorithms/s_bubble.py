3139e76ae261166c3bd6ddb7eebbe49fb8c89ee3
Demky <bb.ItWork@gmail.com>
2018-07-01 17:16:59 +0200

    fixed code

--- algorithms/s_bubble.py
+++ algorithms/s_bubble.py
@@ -3,7 +3,7 @@
 # It compares adjacent items and exchanges those that are out of order.
 # Each pass through the list places the next largest value in its proper place. 
 
-lst = [15, 1, 4, 56, 32, 25, 919, 85, 10, 3]
+lst = [919, 15, 1, 4, 56, 32, 25, 85, 10, 3]
 
 needSorting = True
 i2 = 0
@@ -12,9 +12,9 @@ while needSorting :
     needSorting = False  
     for c in range (0, len(lst)):
         for i2 in range (0, len(lst) - 1 - c): # a sorted number is no more checked
-            if lst[i2] < lst[i2+1]:                    
+            if lst[i2] > lst[i2+1]:                    
                 lst[i2], lst[i2+1] = lst[i2+1], lst[i2] #bubble'd
                 needSorting = True                          
-        print("numbers moving the their right place {}".format(lst))
+                print("sorting in progress -> {}".format(lst)) # numbers moving the their right place
         
-print("final list", lst)
+print("Sorted list : {}".format(lst))
